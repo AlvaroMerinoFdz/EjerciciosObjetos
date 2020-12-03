aacaf9d9c518a062417edf09a1eb53bc27351b16
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2020-12-03 20:16:33 +0100

    Clase contraseña creada

---
+++ Contraseña.py
@@ -0,0 +1,13 @@
+#Queremos programar un generador de password que cumpla las siguientes condiciones:
+#a. Una password debe tener los atributos longitud y contraseña. Por defecto la longitud será 8.
+#b. Podremos generar una contraseña por defecto de longitud 8 o generar una contraseña aleatoria con una longitud dada.
+#c. Deberemos poder comprobar si es fuerte dicha contraseña. Para que una contraseña sea fuerte debe tener más de 2 mayúsculas, más de 1 minúscula y más de 5 números.
+#d. Deberemos poder generar una password, es decir, generar la contraseña del objeto según la longitud que tenga.
+#e. Debemos poder obtener la contraseña y la longitud de una password.
+#f. Debemos poder modificar la longitud de una passsword.
+#Para probarlo:
+#- Crea un array de password con el tamaño solicitado por teclado.
+#- Genera las contraseñas y muestra si son o no fuertes.
+
+if __name__ == '__main__':
+    print("Hola mundo")
