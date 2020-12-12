34c0ebad6075e61cb70860b45a4294fa6ee648b3
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2020-12-12 18:09:42 +0100

    Curso python, continuacion

---
+++ Curso/punto.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Punto():
+    """Representación de un punto en el plano, los atributos son x e y, que representan
+    los valores de las coordenadas cartesianas"""
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def mostrar(self):
+        return str(self.x) + ":" + str(self.y)
+
+    def distancia(self, otro):
+        """Devuelve la distancia entre ambos puntos."""
+        dx = self.x - otro.x
+        dy = self.y - otro.y
+        return math.sqrt(dx * dx + dy * dy)
+if __name__ =='__main__':
+    punto1=Punto()
+    punto2=Punto(4,5)
+    print(punto1.distancia(punto2))
