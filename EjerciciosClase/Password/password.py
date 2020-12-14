b91baba7e00846d8ba33b563dca6ca31158802ed
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2020-12-14 11:08:09 +0100

    Ejercicios de clase empezados, pero no finalizado ya que hay muchas dudas

--- EjerciciosClase/Password/password.py
+++ EjerciciosClase/Password/password.py
@@ -1,44 +1,57 @@
 #!/usr/bin/env python
+"""Queremos programar un generador de password que cumpla las siguientes condiciones:
+a. Una password debe tener los atributos longitud y contraseña. Por defecto la longitud será 8.
+b. Podremos generar una contraseña por defecto de longitud 8 o generar una contraseña aleatoria con una longitud dada.
+c. Deberemos poder comprobar si es fuerte dicha contraseña. Para que una contraseña sea fuerte debe tener más de 2 mayúsculas, más de 1 minúscula y más de 5 números.
+d. Deberemos poder generar una password, es decir, generar la contraseña del objeto según la longitud que tenga.
+e. Debemos poder obtener la contraseña y la longitud de una password.
+f. Debemos poder modificar la longitud de una passsword.
+Para probarlo:
+- Crea un array de password con el tamaño solicitado por teclado.
+- Genera las contraseñas y muestra si son o no fuertes."""
+
 import random
 
 class Password():
-    def __init__(self,contraseña):
-        self.contraseña=contraseña
-        self.longitud = len(contraseña)
+    """CONSTRUCTORES"""
+    def __init__(self,String pwd):
+        self.pwd=str(pwd)
+        self.longitud = len(pwd)
 
     def __init__(self):
-        self.contraseña=random.randint(0, 99999999)
-        self.longitud = len(contraseña)
+        self.pwd=random.randint(0, 99999999)
+        self.longitud = len(self.pwd)
 
-    #GETTERS & SETTERS
+    def __int__(self,longitud):
+        self._pwd = random.randint(0, (10 ^ longitud) - 1)
+
+    """GETTERS & SETTERS"""
     @property
-    def contraseña(self):
-        self._contraseña
+    def pwd(self):
+        self._pwd
 
     @property
     def longitud(self):
         self._longitud
 
-    @contraseña.setter
-    def contraseña(self,contraseña):
-        if len(contraseña)==8:
-            self._contraseña=contraseña
+    @pwd.setter
+    def contraseña(self,pwd):
+        if len(pwd)==8:
+            self._pwd=pwd
         else:
             raise ValueError("Contraseña incorrecta, la longitud tiene que ser de 8 caracteres")
 
 
-    def contraseña(self,longitud):
-        self._contraseña= random.randint(0, (10^longitud)-1)
+    def pwd(self,longitud):
+        self._pwd= random.randint(0, (10^longitud)-1)
 
     @str
     def __str__(self):
-        return self._contraseña+" "
+        return self._pwd+" "
 
-    def comprobar(self,contraseña):
+    def comprobar(self,pwd):
         print("DUDA")
 
 
 if __name__ == "__main__":
-    p1=Password(12345678)
-    #contraseña()
-    #contraseña(12345)
+    p1=Password()
